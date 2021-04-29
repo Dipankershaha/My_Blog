cdc331ebeae305280f678d4efaeefa75accc4616
Dipankershaha <dipankershaha@gmail.com>
2021-04-29 12:14:54 +0600

    heroku deployment settings added

--- My_Blog_Project/urls.py
+++ My_Blog_Project/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from  django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 
@@ -12,6 +14,8 @@ urlpatterns = [
     path('account/', include('App_Login.urls')),
     path('blog/', include('App_Blog.urls')),
     path('', views.Index, name='index'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
